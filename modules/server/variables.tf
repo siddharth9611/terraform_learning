b324f9c7eec748b984bf516ad793a8011bcacca5
siddharth9611 <siddharthhiremath2054@gmail.com>
2023-09-15 20:35:49 +0530

    3rd day commit 15-05-23 modularise the code

---
+++ modules/server/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {}
+variable "my_ip" {}
+variable "env_prefix" {}
+variable "ami_id" {}
+variable "avail_zone" {}
+variable "instance_type" {}
+variable "subnet_id" {}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable tf_vpc_cidr_block{}
+variable tf_subnet_cidr_block{}
+variable avail_zone {}
+variable env_prefix{}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "ami_id" {}
